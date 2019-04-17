bae8428dbb06600cff4d3220f67e9eec8880cd30
MISSNEANDERTHAL <designtmg77@gmail.com>
2019-04-17 16:35:51 -0500

    Counting Columns Code

---
+++ NEWRNN/columncounting.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 11:39:12 2019
+
+@author: owner
+"""
+
+#Create a new function:
+def num_missing(x):
+  return sum(x.isnull())
+
+#Applying per column:
+print "Missing values per column:"
+print data.apply(num_missing, axis=0) #axis=0 defines that function is to be applied on each column
+
+#Applying per row:
+print "\nMissing values per row:"
+print data.apply(num_missing, axis=1).head() #axis=1 defines that function is to be applied on each row
+
+===============================
+
+#Column Variable counts
+#Binning:
+def binning(col, cut_points, labels=None):
+  #Define min and max values:
+  minval = col.min()
+  maxval = col.max()
+
+  #create list by adding min and max to cut_points
+  break_points = [minval] + cut_points + [maxval]
+
+  #if no labels provided, use default labels 0 ... (n-1)
+  if not labels:
+    labels = range(len(cut_points)+1)
+
+  #Binning using cut function of pandas
+  colBin = pd.cut(col,bins=break_points,labels=labels,include_lowest=True)
+  return colBin
+
+#Binning age:
+cut_points = [90,140,190]
+labels = ["low","medium","high","very high"]
+data["LoanAmount_Bin"] = binning(data["LoanAmount"], cut_points, labels)
+print pd.value_counts(data["LoanAmount_Bin"], sort=False)
+
+#https://www.analyticsvidhya.com/blog/2016/01/12-pandas-techniques-python-data-manipulation/
